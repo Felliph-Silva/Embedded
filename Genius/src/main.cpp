a4837ce8d8063d6635e0cbaa4fd98c2783500682
Felliph-Silva <felliphnascimento17@outlook.com>
2024-03-04 23:54:46 -0300

    Add Void setup and compare funcition

--- Genius/src/main.cpp
+++ Genius/src/main.cpp
@@ -1,15 +1,32 @@
 #include <Arduino.h>
+#include <time.h>
 
-// put function declarations here:
-int myFunction(int, int);
+uint8_t Led[] = {0, 1, 2, 3};
+uint8_t buttom[] = {4, 5, 6, 7};
+
+bool compare(uint8_t *sequence_led, uint8_t  *sequence_buttom, uint8_t times) //Function to compare if the sequence of led is equal to the sequence of buttom;
+{
+  for (uint8_t i = 0; i < times; i++)
+  {
+    if (sequence_led[i]!= sequence_buttom[i])
+    {
+      return false;
+    }
+  }
+  return true;
+}
+
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  for(int i = 0; i < 4; i++)
+  {
+    pinMode(Led[i], OUTPUT);
+    pinMode(buttom[i], INPUT_PULLUP);
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  srand(time(NULL));
 }
 
 // put function definitions here:
