0005e5e32bda240e3e894307487d572278783b38
Felliph-Silva <felliphnascimento17@outlook.com>
2024-03-06 17:04:02 -0300

    Almost working

--- Genius/src/main.cpp
+++ Genius/src/main.cpp
@@ -9,10 +9,10 @@
 uint8_t Led[] = {13, 12, 11, 10};
 uint8_t button[] = {5, 4, 3, 2};
 uint8_t round_game = 0;
+uint8_t button_count = 0;
 
 uint8_t Led_sequence[Difficulty_level]; //Storage the Led sequence and button sequence by dynamic allocation
 uint8_t button_sequence[Difficulty_level];
-
 
 bool compare() //Function to compare if the sequence of led is equal to the sequence of button;
 {
@@ -27,7 +27,7 @@ bool compare() //Function to compare if the sequence of led is equal to the sequ
   return true;
 }
 
-void turn_on_Led(uint8_t num) //Function to turn on certain LED based in his position on array.
+void flash_Led(uint8_t num, int time = 1000) //Function to turn on certain LED based in his position on array.
 {
   for (uint8_t i = 0; i < 4; i++)
   {
@@ -37,62 +37,54 @@ void turn_on_Led(uint8_t num) //Function to turn on certain LED based in his pos
     }
     else pinMode(Led[i], LOW);
   }
-  delay(1000);
+  delay(time);
+
+    for (uint8_t i = 0; i < 4; i++)
+    {
+      pinMode(Led[i], LOW);
+    }
+
 }
 
-void add_new_sequence()
+void add_new_sequence() //Add a random number to led sequence
 {
   Led_sequence[round_game] = random(0,4);
 }
 
-
-
-void add_to_sequence(uint8_t *sequence, uint8_t size, int num) //Add a new value to array of sequence
+void save_tap(uint8_t i) //Save the position of Button in the button sequence
 {
-  uint8_t *ptr = sequence;
-  sequence = new uint8_t[size+1];
+  button_sequence[button_count] = i;
+  button_count++;
+}
 
-  for (uint8_t i = 0; i < size; i++)
+void game_over(bool finished) //Reset round in game over
+{
+  if(finished == true)
   {
-    sequence[i] = ptr[i];
+    for (uint8_t i = 0; i < 3; i++)
+    flash_Led(0); 
   }
-  sequence[size+1] = num;
-  delete [] ptr;
-}
 
-/*
-void reset_sequence(uint8_t sequence[]) //Clear sequence
-{
-
-
-}
-*/
-
-void game_over() //Blink the leds in game over
-{
-  round_game= 0;
-  bool state = true;
-  for (uint8_t j = 0; j < 4; ++j)
+  if (finished == true)
   {
-    for (uint8_t i = 0; i < 4; i++)
-    {
-      digitalWrite(Led[i], state);
-    }
-    delay(500);
-    state =!state;
+    for (uint8_t i = 0; i < 3; i++)
+    flash_Led(3); 
   }
+    round_game = 0;
+    button_count = 0;
 }
 
-void pressing_button() //Function that saves button pressed by the user
+void playing() //Function that saves button pressed by the user
 {
   for (uint8_t i = 0; i < 4; i++)
   {
     if (digitalRead(button[i]) == LOW)
     {
-      add_to_sequence(button_sequence, button[i]);
+      save_tap(i);
       while(digitalRead(button[i]) == LOW)
       {
-        turn_on_Led(i);
+        //Implement function to check the time pressing the button "Pressing", with millis;
+        flash_Led(i);
       }
     }
   }
@@ -113,12 +105,45 @@ void setup() {
 
 void loop() {
 
-if (round == 0)
+if (round_game == 0)
   {
     for (uint8_t i = 0; i < 4; i++)
     {
-      turn_on_Led(0);
+      flash_Led(i);
     }
-    delay(1000);
+    round_game++;
+    add_new_sequence();
   }
+
+if (round_game > 0 && round_game <= Difficulty_level)
+{
+  for (uint8_t i = 0; i < round_game; i++)
+  {
+    flash_Led(Led_sequence[i]);
+  }
+
+  playing();
+
+  if (button_count == round_game)
+  {
+    if (compare() == true)
+    {
+      for (uint8_t i = 0; i < 3; i++)
+      {
+        flash_Led(0);
+      }
+      round_game++;
+      button_count =0;
+    }
+    if (compare() == false)
+    {
+      game_over(false);
+    }
+  } 
+}
+
+if (round_game > Difficulty_level)
+{
+  game_over(true);
+}
 }
